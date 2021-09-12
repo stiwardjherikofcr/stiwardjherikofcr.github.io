145157623778e5a01cb37c5a76c077444face156
STIWARD JHERIKOF CARRILLO RAMIREZ <stiwardjherikofcr@ufps.edu.co>
2021-09-11 22:14:36 -0500

    initial project version

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const email = document.getElementById("email")
+const pass = document.getElementById("password")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+
+    if (!regexEmail.test(email.value)) {
+        warnings += 'El email no es valido <br>'
+        entrar = true
+    }
+
+    if (pass.value.length < 8) {
+        warnings += 'La contraseña no es valida <br>'
+        entrar = true
+    }
+
+    if (entrar) {
+        parrafo.innerHTML = warnings
+    }
+})
